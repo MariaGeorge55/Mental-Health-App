5761c9e87da9cbfb0c5e7a4bde1c430305889f99
zahwa-ihab <73435509+zahwa-ihab@users.noreply.github.com>
2020-10-25 23:07:01 +0200

    Add files via upload

---
+++ assets/Onclick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Onclick : MonoBehaviour {
+
+    private void OnMouseDown() ////this function is called when the user has pressed the mouse button while over the Collider.
+    {
+        // Destroy(gameObject);
+        SceneManager.LoadScene(1);
+    }
+}
